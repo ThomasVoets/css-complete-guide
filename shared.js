7689ea8ad3747d0387b3f83899732f16f52d5b71
Thomas Voets <voets_thomas@hotmail.com>
2020-07-17 13:00:34 +0200

    Opening and Closing the Hamburger Menu

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
@@ -10,9 +12,17 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 
 modalNoButton.addEventListener('click', closeModal);
+
+toggleButton.addEventListener('click', function () {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
 
 function closeModal() {
     modal.style.display = 'none';
