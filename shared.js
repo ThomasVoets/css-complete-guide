d1606038f910a882a4ebd83617bb7c2cda1d7b4c
Thomas Voets <voets_thomas@hotmail.com>
2020-07-23 12:31:25 +0200

    Transitions & "display"

--- shared.js
+++ shared.js
@@ -8,13 +8,21 @@ var mobileNav = document.querySelector('.mobile-nav');
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+
+        setTimeout(function () {
+            backdrop.classList.add('open');
+        }, 10);
     });
 }
 
 toggleButton.addEventListener('click', function () {
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+
+    setTimeout(function () {
+        backdrop.classList.add('open');
+    }, 10);
 });
 
 backdrop.addEventListener('click', function () {
@@ -31,4 +39,8 @@ function closeModal() {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+
+    setTimeout(function () {
+        backdrop.style.display = 'none';
+    }, 200);
 }
